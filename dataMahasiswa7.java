63e3f03467fdaf34aad001174687ec6a62299109
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-04-16 17:47:21 +0700

    baru

--- dataMahasiswa7.java
+++ dataMahasiswa7.java
@@ -1,13 +1,13 @@
 public class dataMahasiswa7 {
 
-    // Method to display all students
+    // Method untuk menampilkan data mahasiswa
     public void tampilkanMahasiswa(Mahasiswa7[] arrMhs7) {
         for (Mahasiswa7 mhs : arrMhs7) {
             System.out.println("Nama: " + mhs.nama + ", NIM: " + mhs.nim + ", Prodi: " + mhs.prodi);
         }
     }
 
-    // Method to search for a student by NIM
+    // Method untuk mencari data mahasiswa berdasarkan NIM
     public void cariMahasiswa(Mahasiswa7[] arrMhs7, String nim) {
         for (Mahasiswa7 mhs : arrMhs7) {
             if (mhs.nim.equals(nim)) {
