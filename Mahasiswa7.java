a99cee10f4b7b1f6bd795d63dbe5f94ba6cab7b4
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-04-19 20:00:52 +0700

    update malam

--- Mahasiswa7.java
+++ Mahasiswa7.java
@@ -13,7 +13,7 @@ public class Mahasiswa7 {
     }
 
     void tampilkanMahasiswa() {
-        System.out.println(nama + "\t|" + nim + "| \t" + prodi);
+        System.out.printf("%-15s| %-15s | %-15s |%n", nama, nim, prodi);
     }
 }
 
